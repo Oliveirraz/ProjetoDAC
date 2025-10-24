35d5344806c6f102d890287a02432187a0747e59
Glêisson de Oliveira Bezerra <oliveirabezerraeu@gmail.com>
2025-10-24 20:23:45 -0300

    primeiro commit

---
+++ src/main/java/com/InAula/InAula/InAulaApplication.java
@@ -0,0 +1,13 @@
+package com.InAula.InAula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InAulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InAulaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/InAula/InAula/InAulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InAula.InAula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InAulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
